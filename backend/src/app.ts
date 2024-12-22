dc8abcf12df552923d77a01c50c08d1310c187d3
AdityaTote <adityatote24@gmail.com>
2024-12-22 16:29:58 +0530

    some changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,32 +5,11 @@ import cors from "cors";
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    // origin: ["http://localhost:5173", "http://localhost:4173", "https://course-hub-azure.vercel.app", "https://76.76.21.93:443"],
-    origin: "https://course-hub-azure.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', 'https://course-hub-azure.vercel.app');
-  res.header('Access-Control-Allow-Credentials', "true");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
+
 
 // Routes
 import { secureAdminRoute } from "./routes/secure.routes";
